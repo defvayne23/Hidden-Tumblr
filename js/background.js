0d07091f60bd8cb58c5e8a840f2634bc38036d6d
James Fleeting <hello@jamesfleeting.com>
2014-04-24 11:21:40 -0500

    Added page_action that links to the options page if on tumblr.com.

--- js/background.js
+++ js/background.js
@@ -8,3 +8,22 @@ chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
 		sendResponse({});
 	}
 });
+
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { urlContains: 'tumblr.com' },
+          })
+        ],
+        actions: [ new chrome.declarativeContent.ShowPageAction() ]
+      }
+    ]);
+  });
+});
+
+chrome.pageAction.onClicked.addListener(function(tab) {
+  chrome.tabs.create({url: "options.html"});
+});
